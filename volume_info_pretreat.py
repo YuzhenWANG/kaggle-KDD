31b5ec406a394bd8a6f246ca1fea5905876617a5
liuyingqi <billliuyingqi@126.com>
2017-04-13 15:21:37 +0800

    delete useless file

--- volume_info_pretreat.py
+++ volume_info_pretreat.py
@@ -7,120 +7,146 @@ from datetime import datetime,timedelta
 from matplotlib import pyplot
 import seaborn
 import statsmodels.api as sm
+from sklearn.model_selection import KFold
 
-# index = pd.date_range('19/9/2016', periods=(4*7+1)*24*3, freq='20T')
-# print index
-#
-# info = pd.read_csv('input/volume(table 6)_training.csv')
-# vehicle_models = info['vehicle_model'].unique()
-# print vehicle_models
-#
-# tollgate_id = info['tollgate_id'].unique()
-# volume_info = np.zeros(((4*7+1)*24*3,len(vehicle_models)*5),dtype=int)
-# print volume_info.shape
-#
-# start = '2016-09-19 00:00:00'
-# FMT = "%Y-%m-%d %H:%M:%S"
-# start = datetime.strptime(start, FMT)
-#
-# tollgate_dict = {(1,0):0,(1,1):1,(2,0):2,(3,0):3,(3,1):4}
-#
-# # read line by line and hash table into relative matrix
-# fr = open('input/volume(table 6)_training.csv', 'r')
-# fr.readline()
-# txt = fr.readlines()  # skip the header
-# fr.close()
-# for str_line in txt:
-#     str_line = str_line.replace('"', '').split(',')
-#     tollgate_id = str_line[1]
-#     direction = str_line[2]
-#     vehile_model = str_line[3]
-#     pass_time = str_line[0]
-#
-#     pass_time = datetime.strptime(pass_time, "%Y-%m-%d %H:%M:%S")
-#
-#     # calculating time delta
-#     print str_line
-#     delta = pass_time - start
-#     day_diff = delta.days
-#     hour_diff =  delta.seconds//3600
-#     minute_diff = (delta.seconds % 3600) // 60
-#     total_diff_minute = day_diff*24*60 + hour_diff*60 + minute_diff
-#     row_num = total_diff_minute // 20
-#
-#     # calculating column num
-#     column_num = int(vehile_model)*5 + tollgate_dict[(int(tollgate_id),int(direction))]
-#
-#     volume_info[row_num,column_num]+=1
-# np.savetxt('volume.csv',volume_info,fmt='%d')
+# step 1 : change table 6 rural data into 20 minute time window and calculate them by vehicle model
+# input: table 6 output: matrix 2088 * 40 (8 vehicle models and each 5 tollgate)
+#########################################################################################################
 
-f = open('volume.csv','r')
-info = np.loadtxt(f,dtype=int)
-print info.shape
+print 'step1 : change table 6 into matrix'
+index = pd.date_range('19/9/2016', periods=(4*7+1)*24*3, freq='20T')
+print index
 
-# i = 14
-# for i in range(35):
-#     print i
-#     pyplot.figure()
-#     # pyplot.plot(info[:,i]+info[:,i+10]+info[:,i+5]+info[:,i+15]+info[:,i+20])
-#     pyplot.plot(info[:,i])
-#     pyplot.show()
+info = pd.read_csv('input/volume(table 6)_training.csv')
+vehicle_models = info['vehicle_model'].unique()
+print vehicle_models
 
+tollgate_id = info['tollgate_id'].unique()
+volume_info = np.zeros(((4*7+1)*24*3,len(vehicle_models)*5),dtype=int)
+print volume_info.shape
 
-############################################################
-#  add  relative day feature
-# relative_day_in_week = []
-# relative_day_in_week.extend([2]*72) # 19
-# relative_day_in_week.extend([2]*72) # 20
-# relative_day_in_week.extend([3]*72) # 21
-# relative_day_in_week.extend([4]*72) # 22
-# relative_day_in_week.extend([5]*72) # 23
-# relative_day_in_week.extend([6]*72) # 24
-# relative_day_in_week.extend([7]*72) # 25
-# relative_day_in_week.extend([1]*72) # 26
-# relative_day_in_week.extend([2]*72) # 27
-# relative_day_in_week.extend([3]*72) # 28
-# relative_day_in_week.extend([4]*72) # 29
-# # delete 9 days
-# relative_day_in_week.extend([0]*72*9)
-#
-# relative_day_in_week.extend([2]*72) # 9
-# relative_day_in_week.extend([2]*72) # 10
-# relative_day_in_week.extend([3]*72) # 11
-# relative_day_in_week.extend([3]*72) # 12
-# relative_day_in_week.extend([4]*72) # 13
-# relative_day_in_week.extend([5]*72) # 14
-# relative_day_in_week.extend([6]*72) # 15
-# relative_day_in_week.extend([7]*72) # 16
-# relative_day_in_week.extend([1]*72) # 17
-#
-# print len(relative_day_in_week)
+start = '2016-09-19 00:00:00'
+FMT = "%Y-%m-%d %H:%M:%S"
+start = datetime.strptime(start, FMT)
 
-model_1_volume = info[:,5:10]
-model_2_volume = info[:,10:15]
-model_extra_volume = info[:,0:5] + info[:,15:20] + info[:,20:25] + info[:,25:30] + info[:,30:35] + info[:,35:]
+tollgate_dict = {(1,0):0,(1,1):1,(2,0):2,(3,0):3,(3,1):4}
+
+# read line by line and hash table into relative matrix
+fr = open('input/volume(table 6)_training.csv', 'r')
+fr.readline()
+txt = fr.readlines()  # skip the header
+fr.close()
+for str_line in txt:
+    str_line = str_line.replace('"', '').split(',')
+    tollgate_id = str_line[1]
+    direction = str_line[2]
+    vehile_model = str_line[3]
+    pass_time = str_line[0]
+
+    pass_time = datetime.strptime(pass_time, "%Y-%m-%d %H:%M:%S")
+
+    # calculating time delta
+    delta = pass_time - start
+    day_diff = delta.days
+    hour_diff =  delta.seconds//3600
+    minute_diff = (delta.seconds % 3600) // 60
+    total_diff_minute = day_diff*24*60 + hour_diff*60 + minute_diff
+    row_num = total_diff_minute // 20
+
+    # calculating column num
+    column_num = int(vehile_model)*5 + tollgate_dict[(int(tollgate_id),int(direction))]
+
+    volume_info[row_num,column_num]+=1
+np.savetxt('volume_files/volume_training_set.csv',volume_info,fmt='%d')
+print volume_info.shape
+
+# step2: delete national day 9 days
+# seperate total volume into 3 part (model1 , model2 , model extra)
+#######################################################################################
+
+print 'step 2 : delete national days '
+
+model_1_volume = volume_info[:,5:10]
+model_2_volume = volume_info[:,10:15]
+model_extra_volume = volume_info[:,0:5] + volume_info[:,15:20] + volume_info[:,20:25] + volume_info[:,25:30] + volume_info[:,30:35] + volume_info[:,35:]
 model_1_volume = np.vstack((model_1_volume[:792,:],model_1_volume[-648:,:]))
 model_2_volume = np.vstack((model_2_volume[:792,:],model_2_volume[-648:,:]))
 model_extra_volume = np.vstack((model_extra_volume[:792,:],model_extra_volume[-648:,:]))
 print model_1_volume.shape
 print model_2_volume.shape
 print model_extra_volume.shape
+np.savetxt('volume_files/volume_model1_trainset.csv',model_1_volume,fmt='%d')
+np.savetxt('volume_files/volume_model2_trainset.csv',model_2_volume,fmt='%d')
+np.savetxt('volume_files/volume_modelextra_trainset.csv',model_extra_volume,fmt='%d')
 
 # pyplot.figure()
 # pyplot.plot(model_extra_volume)
 # pyplot.show()
 
-# save seasonal (weekly) feature
-seasonal_matrix = np.empty((0,1440))
-for i in range(5):
-    res = sm.tsa.seasonal_decompose(model_1_volume[:,i],freq=72)
-    seasonal_matrix = np.vstack((seasonal_matrix,res.seasonal))
-for i in range(5):
-    res = sm.tsa.seasonal_decompose(model_2_volume[:,i],freq=72)
-    seasonal_matrix = np.vstack((seasonal_matrix,res.seasonal))
-for i in range(5):
-    res = sm.tsa.seasonal_decompose(model_extra_volume[:, i], freq=72)
-    seasonal_matrix = np.vstack((seasonal_matrix, res.seasonal))
+# step3: prepare test set 1
+# save testset1 into 3 (7 * 24 *3, 5) matrix
+#################################################################################
 
-print seasonal_matrix
-np.savetxt('volumn_seasonal_feature.csv',seasonal_matrix[:,:24*3],fmt='%f')
+print 'step 3 prepare test set 1 into 3 matrix '
+volume_test_1 = np.zeros((7*24*3,len(vehicle_models)*5),dtype=int)
+print volume_test_1.shape
+
+start = '2016-10-18 00:00:00'
+FMT = "%Y-%m-%d %H:%M:%S"
+start = datetime.strptime(start, FMT)
+
+fr = open('input/volume(table 6)_test1.csv', 'r')
+fr.readline()
+txt = fr.readlines()  # skip the header
+fr.close()
+for str_line in txt:
+    str_line = str_line.replace('"', '').split(',')
+    tollgate_id = str_line[1]
+    direction = str_line[2]
+    vehile_model = str_line[3]
+    pass_time = str_line[0]
+
+    pass_time = datetime.strptime(pass_time, "%Y-%m-%d %H:%M:%S")
+
+    # calculating time delta
+    # print str_line
+    delta = pass_time - start
+    day_diff = delta.days
+    hour_diff =  delta.seconds//3600
+    minute_diff = (delta.seconds % 3600) // 60
+    total_diff_minute = day_diff*24*60 + hour_diff*60 + minute_diff
+    row_num = total_diff_minute // 20
+
+    # calculating column num
+    column_num = int(vehile_model)*5 + tollgate_dict[(int(tollgate_id),int(direction))]
+    volume_test_1[row_num,column_num]+=1
+
+model_1_volume_testset_1 = volume_test_1[:,5:10]
+model_2_volume_testset_1 = volume_test_1[:,10:15]
+model_extra_volume_testset_1 = volume_test_1[:,0:5] + volume_test_1[:,15:20] + volume_test_1[:,20:25] + volume_test_1[:,25:30] + volume_test_1[:,30:35] + volume_test_1[:,35:]
+
+np.savetxt('volume_files/volume_model1_test_set_1.csv',model_1_volume_testset_1,fmt='%d')
+np.savetxt('volume_files/volume_model2_test_set_1.csv',model_2_volume_testset_1,fmt='%d')
+np.savetxt('volume_files/volume_modelextra_test_set_1.csv',model_extra_volume_testset_1,fmt='%d')
+print model_1_volume_testset_1.shape
+print model_2_volume_testset_1.shape
+print model_extra_volume_testset_1.shape
+
+# pyplot.figure()
+# pyplot.plot(volume_test_1)
+# pyplot.show()
+
+# step 4: create n fold fies:
+# change 20 days data into 5 fold train and test set
+# generating train test files
+#####################################################################
+
+days = range(20)
+relative_weed = [2,2,3,4,5,6,7,1,2,3,4,2,2,3,3,4,5,6,7,1]
+
+kf = KFold(n_splits=5,random_state = 5)
+for train_index,test_index in kf.split(days):
+    print train_index,test_index
+
+
+
