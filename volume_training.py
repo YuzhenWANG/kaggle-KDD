7cdbfa5e8a2e6db2514e095fb30794ff617cfed3
liuyingqi <billliuyingqi@126.com>
2017-04-14 11:15:45 +0800

    first version of CV test

--- volume_training.py
+++ volume_training.py
@@ -47,6 +47,8 @@ print relative_day_trainset
 print relative_day_CV_set
 print relative_day_testset
 
+print 'start training .............................'
+
 # step 1 : find seasonal part and remove seasonnal part
 print 'step 1: sesonal weekly model'
 trainset = np.loadtxt('volume_files/fold1/model_1_trainset.csv',dtype=int)
@@ -55,6 +57,8 @@ for i in range(5):
     res = sm.tsa.seasonal_decompose(trainset[:,i],freq=72)
     seasonal_part = np.vstack((seasonal_part,res.seasonal[:72*7]))
     trainset[:,i] = trainset[:,i] - res.seasonal
+seasonal_part = np.transpose(seasonal_part)
+print seasonal_part
 print seasonal_part.shape
 print trainset.shape
 
@@ -82,6 +86,23 @@ print relative_weekday_part.shape
 
 trainset = trainset - relative_weekday_part
 
-pyplot.figure()
-pyplot.plot(trainset)
-pyplot.show()
+# CV prediction
+CV_seasonal_part = seasonal_part[:4*72,:]
+print CV_seasonal_part
+CV_relative_weedday_part = np.ones((4*72,5),dtype=float)
+for i in range(5):
+    CV_relative_weedday_part[:,i] = CV_relative_weedday_part[:,i]*mean_value[i]
+    for j,num in zip(CV_set_days,range(len(CV_set_days))):
+        CV_relative_weedday_part[num*72:(num+1)*72,i] = (1+alpha_model_value[j-1,i]) * CV_relative_weedday_part[num*72:(num+1)*72,i]
+print CV_relative_weedday_part
+
+# CV test
+CV_set = np.loadtxt('volume_files/fold1/model_1_CV_set.csv',dtype=int)
+
+CV_set_prediction = CV_seasonal_part + CV_relative_weedday_part
+for i in range(5):
+    pyplot.figure()
+    pyplot.plot(CV_set[:,i])
+    pyplot.plot(CV_set_prediction[:,i])
+    pyplot.show()
+
