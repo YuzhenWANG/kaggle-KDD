94c711e66bcac33c9e1c0df0eaa663ab740a7b21
Yuzhen <Yuzhen0915@gmail.com>
2017-04-22 09:53:02 +0800

    version corrected

--- average_relative_weekday.py
+++ average_relative_weekday.py
@@ -2,43 +2,22 @@ import pandas as pd
 import numpy as np
 
 volume1=np.loadtxt('volume_files/volume_model1_trainset.csv')
+volume2=np.loadtxt('volume_files/volume_model2_trainset.csv')
+volume3=np.loadtxt('volume_files/volume_modelextra_trainset.csv')
 submission_table = pd.read_csv('submission_sample_volume.csv')
 prediction=np.zeros((7*72,5),dtype=float)
 relative_weekday = [2,2,3,4,5,6,7,1,2,3,4,2,2,3,3,4,5,6,7,1]
-predict_weekday=[2,3,4,5,6,7,1]
+predict_weekday=[3,4,5,6,7,1,2]
 
-print type(volume1)
-print volume1.shape
-
-
-for model in range(5):
-    volume=volume1[:,model]
-    # day = 3
-    for i in range(72):
-        prediction[i,model]=(volume[2*72+i]+volume[9*72+i]+volume[13*72+i]+volume[14*72+i])/4
-
-
-    # day = 4
-    for i in range(72):
-        prediction[72+i,model]=(volume[3*72+i]+volume[10*72+i]+volume[15*72+i])/3
-    # day = 5
-    for i in range(72):
-        prediction[2*72+i,model]=(volume[4*72+i]+volume[16*72+i])/2
-    # day = 6
-    for i in range(72):
-        prediction[3*72+i,model]=(volume[5*72+i]+volume[17*72+i])/2
-    # day = 7
-    for i in range(72):
-        prediction[4*72+i,model]=(volume[6*72+i]+volume[18*72+i])/2
-    # day = 1
-    for i in range(72):
-        prediction[5*72+i,model]=(volume[7*72+i]+volume[19*72+i])/2
-    # day = 2
-    for i in range(72):
-        prediction[6*72+i,model]=(volume[0*72+i]+volume[1*72+i]+volume[8*72+i]+volume[11*72+i]+volume[12*72+i])/5
-
-print prediction
-print prediction.shape
+# i day correspond index= [i*72:(i+1)*72]
+for volume in [volume1,volume2,volume3]:
+    prediction[:72]+=(volume[2*72:(2+1)*72]+volume[9*72:(9+1)*72]+volume[13*72:(13+1)*72]+volume[14*72:(14+1)*72])/4 #day 3
+    prediction[72:2*72]+=(volume[3*72:(3+1)*72]+volume[10*72:(10+1)*72]+volume[15*72:(15+1)*72])/3 #day 4
+    prediction[2*72:3*72]+=(volume[4*72:(4+1)*72]+volume[16*72:(16+1)*72])/2  #day 5
+    prediction[3*72:4*72] += (volume[5 * 72:(5 + 1) * 72] + volume[17 * 72:(17 + 1) * 72]) / 2  # day 6
+    prediction[4 * 72:5 * 72] += (volume[6 * 72:(6 + 1) * 72] + volume[18 * 72:(18 + 1) * 72]) / 2  # day 7
+    prediction[5 * 72:6 * 72] += (volume[7 * 72:(7 + 1) * 72] + volume[19 * 72:(19 + 1) * 72]) / 2  # day 1
+    prediction[6 * 72:7 * 72]+=(volume[0*72:(0+1)*72]+volume[1*72:(1+1)*72]+volume[8*72:(8+1)*72]+volume[11*72:(11+1)*72]+volume[12*72:(12+1)*72])/5  # day2
 
 
 # generating submission files
@@ -62,4 +41,5 @@ print submission.shape
 submission_table['volume'] = submission
 
 print submission_table[:15]
-submission_table.to_csv('volume_submission_relativeday.csv',index=False)
+submission_table.to_csv('volume_submission_relativeday_2.csv',index=False)
+
